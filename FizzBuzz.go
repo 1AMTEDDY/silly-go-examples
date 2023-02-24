8871749d3ba41fa57035a6605c51f7a043ff1118
Peterson Henderson <teddy@teddys-Pro.local>
2023-02-24 19:25:39 +0100

    added exercism playground

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -11,11 +11,11 @@ func main() {
 	for num := 0; num <= 100; num++ {
 		if num%15 == 0 {
 			fmt.Println("FizzBuzz")
-		} else if num%5 == 0 {
-			fmt.Println("Buzz")
-
 		} else if num%3 == 0 {
 			fmt.Println("Fizz")
+
+		} else if num%5 == 0 {
+			fmt.Println("Buzz")
 		} else {
 			fmt.Println(num)
 		}
--- loop.go
+++ loop.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	num := 13
 	fmt.Println(5 > num || num == 13) //this is an "OR"(||) statement only one statement needs to be true for all statement to be true
-	fmt.Println(5 > num && num == 13) // this is an "AND" (&&)statement only one statement needs to be flase for all statement to be
+	fmt.Println(5 > num && num == 13) // this is an "AND" (&&)statement only one statement needs to be false for all statement to be
 
 }
